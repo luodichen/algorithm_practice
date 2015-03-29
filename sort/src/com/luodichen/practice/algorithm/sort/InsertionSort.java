f12c25703382cfc55e8fa736210fc5c52979718c
luodichen <me@luodichen.com>
2015-03-30 02:11:47 +0800

    tiny modify

--- sort/src/com/luodichen/practice/algorithm/sort/InsertionSort.java
+++ sort/src/com/luodichen/practice/algorithm/sort/InsertionSort.java
@@ -6,13 +6,9 @@ public class InsertionSort<T extends Comparable<T>> implements ISort<T> {
     public void sort(T[] a) {
         int length = a.length;
         
-        for (int i = 0; i < length; i++) {
-            int cur = i;
-            while (cur > 0 && a[cur].compareTo(a[cur - 1]) < 0) {
+        for (int i = 0; i < length; i++) 
+            for (int cur = i; cur > 0 && a[cur].compareTo(a[cur - 1]) < 0; cur--) 
                 swap(a, cur, cur - 1);
-                --cur;
-            }
-        }
     }
     
     private void swap(T[] a, int i, int j) {
