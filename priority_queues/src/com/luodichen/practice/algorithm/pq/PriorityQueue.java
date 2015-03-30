86a55b868faa2a227c2d08c4029b21132109585b
luodichen <me@luodichen.com>
2015-03-31 02:30:28 +0800

    inital files of priority queues

---
+++ priority_queues/src/com/luodichen/practice/algorithm/pq/PriorityQueue.java
@@ -0,0 +1,13 @@
+package com.luodichen.practice.algorithm.pq;
+
+public abstract class PriorityQueue<T> {
+    protected abstract int compare(T left, T right);
+    
+    public T first() {
+        return null;
+    }
+    
+    public T delFirst() {
+        return null;
+    }
+}
