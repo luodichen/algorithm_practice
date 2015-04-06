e39c3ef4858bdd2cc9ceeed1d506acffd06b9f9c
luodichen <me@luodichen.com>
2015-04-06 23:51:07 +0800

    add sink and swim method

--- priority_queues/src/com/luodichen/practice/algorithm/pq/PriorityQueue.java
+++ priority_queues/src/com/luodichen/practice/algorithm/pq/PriorityQueue.java
@@ -11,12 +11,7 @@ public abstract class PriorityQueue<T> {
     protected abstract int compare(T left, T right);
     
     public void insert(T v) {
-        int index = mHeap.insert(v);
-        while (null != mHeap.get(index / 2) 
-                && compare(mHeap.get(index), mHeap.get(index / 2)) < 0) {
-            swap(index, index / 2);
-            index /= 2;
-        }
+        swim(mHeap.insert(v));
     }
     
     public T first() {
@@ -36,9 +31,28 @@ public abstract class PriorityQueue<T> {
         }
         
         mHeap.set(1, back);
+        sink(1);
         
+        return ret;
+    }
+    
+    private void swap(int i, int j) {
+        T tmp = mHeap.get(i);
+        mHeap.set(i, mHeap.get(j));
+        mHeap.set(j, tmp);
+    }
+    
+    private void swim(int index) {
+        while (null != mHeap.get(index / 2) 
+                && compare(mHeap.get(index), mHeap.get(index / 2)) < 0) {
+            swap(index, index / 2);
+            index /= 2;
+        }
+    }
+    
+    private void sink(int index) {
         int size = mHeap.size();
-        int index = 1;
+        
         while (index * 2 < size) {
             T parent = mHeap.get(index);
             index *= 2;
@@ -52,13 +66,5 @@ public abstract class PriorityQueue<T> {
                 break;
             }
         }
-        
-        return ret;
-    }
-    
-    private void swap(int i, int j) {
-        T tmp = mHeap.get(i);
-        mHeap.set(i, mHeap.get(j));
-        mHeap.set(j, tmp);
     }
 }
